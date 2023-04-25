537b7e2faa1e6cbc4d2feddc6c62dbb7ac1b4187
karti0411 <123241653+karti0411@users.noreply.github.com>
2023-04-25 12:45:10 +0530

    Add files via upload

---
+++ BalancedBracket.java
@@ -0,0 +1,72 @@
+package balanced_bracket;
+
+import java.util.Stack;
+
+public class BalancedBracket {
+	
+	public static Boolean isBalanced(String exp) {
+		
+		Stack<Character> stack = new Stack<Character>();
+		
+        char[] charArray = exp.toCharArray();   
+        
+        for (int i = 0; i < charArray.length; i++) {
+			
+            char currChar = charArray[i];   
+            
+            if (currChar == '{' || currChar == '[' || currChar == '(') {   
+                stack.push(currChar);   
+                continue;   
+            }   
+            
+            if (stack.isEmpty()) {    
+                return false;   
+            }   
+            
+            char popChar;   
+
+            switch (currChar) {
+			case ')':
+                popChar = (char) stack.pop();   
+                
+                if (popChar == '{' || popChar == '[')   
+                    return false;   
+                
+				break;
+				
+			case '}':   
+                popChar = (char) stack.pop();   
+                if (popChar == '(' || popChar == '[')   
+                    return false;   
+                break;   
+                case ']':   
+                popChar = (char) stack.pop();   
+                if (popChar == '(' || popChar == '{')   
+                    return false;   
+                break;  
+
+			default:
+				break;
+			}
+
+		}
+
+        if (stack.isEmpty()) {
+        	return true;
+        }
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		
+		String exp = "(({}))";
+		
+		if (BalancedBracket.isBalanced(exp))   
+            System.out.println("The entered String has Balanced Brackets.");   
+        else   
+            System.out.println("The entered Strings do not contain Balanced Brackets.");   
+		
+	}
+
+}
